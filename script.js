7eeea893a07654b9d8e9fde4e92f000092022d04
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2022-12-21 08:13:35 +0100

    fixes

--- script.js
+++ script.js
@@ -4,34 +4,43 @@ let xx = `30 DAYS OF JAVASCRIPT CHALLENGE 2020 ASABENEH YETAYEH`
 let sp = xx.split('')
 h1.style.display = 'flex'
 h1.style.flexWrap = 'wrap'
-let pp = document.querySelectorAll('p')
-const foreach = pp.forEach((p) => {
-    for (let i = 0; i < p.length; i++) {
-        console.log(p[i])
-        const func = () => {
 
-            let color = '#' + Math.random().toString(16).substr(2, 6)
-            p[i].style.color = color
-        }
-        setInterval(func, 1000)
-    }
-})
+
+
 for (let i = 0; i < xx.length; i++) {
+
+    let container = document.createElement('div')
+
+    container.style.display = 'flex'
+    container.style.flexWrap = 'wrap'
+    container.style.gap = '1rem'
     let h2 = document.createElement('h1')
     h2.textContent = xx[i]
     console.log(xx[i])
-    h1.appendChild(h2)
+    container.appendChild(h2)
+    h1.appendChild(container)
+
 
     if (i == 1 || i == 6 || i == 9 || i == 20 || i == 30 || i == 35 || i == 44) {
-        h2.style.marginRight = '3rem'
+        h2.style.marginRight = '1.5rem'
     }
     const func = () => {
         let color = '#' + Math.random().toString(16).substr(2, 6)
-        h2.style.color = color
+        container.style.color = color
+        container.style.transition = 'all 1s linear'
+
     }
     setInterval(func, 1000)
 }
 
+
+
+const func = () => {
+    let color = '#' + Math.random().toString(16).substr(2, 6)
+    main.style.backgroundColor = color
+    main.style.transition = 'all 1s linear'
+}
+setInterval(func, 1000)
 
 
 
@@ -39,3 +48,36 @@ for (let i = 0; i < xx.length; i++) {
 const changeColor = () => {
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < p.length; i++) {
+//     console.log(p[i])
+//     const func = () => {
+
+//         let color = '#' + Math.random().toString(16).substr(2, 6)
+//         p[i].style.color = color
+//     }
+//     setInterval(func, 1000)
+// }
